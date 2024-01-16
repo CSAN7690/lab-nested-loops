46edb01314aa6b2ae5a9f35c9d9707f0aee97260
CSAN7690 <christophersanchez@pursuit.org>
2024-01-16 12:22:33 -0500

    Complete problem 1️⃣

--- index.js
+++ index.js
@@ -8,7 +8,16 @@
  * countZeroes([[1, 0, 3], [4, 5, 0], [7, 8, 9]])
  */
 function countZeroes(matrix) {
-  // Function implementation.
+  // Function implementation
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        count++;
+      }
+    }
+  }
+  return count;
 }
 
 
@@ -70,9 +79,9 @@ function calculateGridFunctions(grid) {
 
 
 module.exports = {
-    countZeroes,
-    findElement,
-    logGridCoordinates,
-    organizeSchedule,
-    calculateGridFunctions
+  countZeroes,
+  findElement,
+  logGridCoordinates,
+  organizeSchedule,
+  calculateGridFunctions
 }
